072628dab747ac2eadc4ce927111b4ad114a0eff
Ruslan <ruslan.vanin@pm.bet>
2022-03-26 16:16:25 +0200

    base clicker contract ligic is added

--- contracts/Clicker.sol
+++ contracts/Clicker.sol
@@ -12,8 +12,19 @@ contract Clicker {
 
     event ValueChanged(uint256 indexed newValue);
     event UserInited(address indexed userAddress);
+    event ValuePerClickUpgraded(uint256 indexed userAddress);
 
-    function initUser() public {
+    modifier onlyInitedUser() {
+        require(initedUsers[msg.sender] == true, "Only inited users can do it");
+        _;
+    }
+
+    modifier onlyNotInitedUser() {
+        require(initedUsers[msg.sender] == false, "Only not inited users can do it");
+        _;
+    }
+
+    function initUser() public onlyNotInitedUser {
         valuePerClick[msg.sender] = 1;
         costToUpValuePerClickV1[msg.sender] = 5;
         costToUpValuePerClickV2[msg.sender] = 10;
@@ -23,14 +34,34 @@ contract Clicker {
         emit UserInited(msg.sender);
     }
 
-    function getUserData() public view returns (uint256, uint256, uint256, uint256) {
+    function getUserData() public view onlyInitedUser returns (uint256, uint256, uint256, uint256) {
         return (balances[msg.sender], valuePerClick[msg.sender], costToUpValuePerClickV1[msg.sender], costToUpValuePerClickV2[msg.sender]);
     }
 
-    function click() public {
+    function click() public onlyInitedUser {
         uint256 newValue = valuePerClick[msg.sender];
 
         balances[msg.sender] += newValue;
         emit ValueChanged(newValue);
+    }
+
+    function upgradeValuePerClick(string memory variation) public onlyInitedUser {
+        if(keccak256(abi.encodePacked((variation))) == keccak256(abi.encodePacked(('V1')))) {
+            require(costToUpValuePerClickV1[msg.sender] <= balances[msg.sender], "not enough tokens to upgrade first variation");
+
+            balances[msg.sender] -= costToUpValuePerClickV1[msg.sender];
+            costToUpValuePerClickV1[msg.sender] *= 2;
+            valuePerClick[msg.sender] += 1;
+        }
+
+        if(keccak256(abi.encodePacked((variation))) == keccak256(abi.encodePacked(('V2')))){
+            require(costToUpValuePerClickV1[msg.sender] <= balances[msg.sender], "not enough tokens to upgrade second variation");
+
+            balances[msg.sender] -= costToUpValuePerClickV2[msg.sender];
+            costToUpValuePerClickV2[msg.sender] *= 2;
+            valuePerClick[msg.sender] += 2;
+        }
+
+        emit ValuePerClickUpgraded(valuePerClick[msg.sender]);
     }
 }
